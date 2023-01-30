ef1a08fbb407c2e0370af9f66eb7cd2aa4255aeb
Caroline Wood <carowood@egmail.com>
2023-01-30 18:55:25 +0000

    Updated day 2

--- dec02/index.js
+++ dec02/index.js
@@ -7,42 +7,68 @@ const rounds = puzzleInput.trim().split("\n").map(round=>{
     return round.split(" ");
 });
 
-const getTotalScore = (rounds) => {
-    let total = 0;
-    rounds.map(round => {
-        let gameScore = 0;
-        /*  
+const getTotalScorePart1 = (rounds) => {
+    /*  
             OPPONENT:
             A = rock       
             B = paper      
             C = scissors    
         
-            ME:
+            MY SHAPE VALUES:
             X = rock        => 1
             Y = paper       => 2
             Z = scissors    => 3
 
+            ROUND SCORES:
             lose            => 0
             draw            => 3
             win             => 6
         */
-        if(round[0] === "A" && round[1] === "X") gameScore = 4 // draw (3 pt) + rock (1 pt)
-        if(round[0] === "A" && round[1] === "Y") gameScore = 8 // win (6 pt) + paper (2 pt)
-        if(round[0] === "A" && round[1] === "Z") gameScore = 3 // lose (0 pt) + scissors (3 pt)
-        if(round[0] === "B" && round[1] === "X") gameScore = 1 // lose (0 pt) + rock (1 pt)
-        if(round[0] === "B" && round[1] === "Y") gameScore = 5 // draw (3 pt) + paper (2 pt)
-        if(round[0] === "B" && round[1] === "Z") gameScore = 9 // win (6 pt) + scissors (3 pt)
-        if(round[0] === "C" && round[1] === "X") gameScore = 7 // win (6 pt) + rock (1 pt)
-        if(round[0] === "C" && round[1] === "Y") gameScore = 2 // lose (0 pt) + paper (2 pt)
-        if(round[0] === "C" && round[1] === "Z") gameScore = 6 // draw (3 pt) + scissors (3 pt)    
-        total = total + gameScore;  
-    });
+
+    let total = 0;
+
+    const roundScores = {
+        A: { X: 3, Y: 6, Z: 0 },
+        B: { X: 0, Y: 3, Z: 6 },
+        C: { X: 6, Y: 0, Z: 3 },
+    };
+    
+    const myShapeValues = { X: 1, Y: 2, Z: 3 };
+    
+    for (const [a, b] of rounds) {
+        total += roundScores[a][b] + myShapeValues[b];
+    };
+
     return total;
 };
 
-const answer = getTotalScore(rounds);
+const getTotalScorePart2 = (rounds) => {
+    let total = 0;
+   
+    const shapeValues = {
+        A: { X: 3, Y: 1, Z: 2 },
+        B: { X: 1, Y: 2, Z: 3 },
+        C: { X: 2, Y: 3, Z: 1 }
+    };
+
+    const roundScores = { X: 0, Y: 3, Z: 6 };
+    
+    for (const [a, b] of rounds) {
+        total += roundScores[b] + shapeValues[a][b]
+    }
+
+    return total;
+};
+
+const answer1 = getTotalScorePart1(rounds);  
+const answer2 = getTotalScorePart2(rounds);
 
 console.log(`
     What would your total score be if everything goes exactly according to your strategy guide? 
-    ${answer}
+    ${answer1}
     `);
+console.log(`
+    Following the Elf's instructions for the second column, what would your total score be if 
+    everything goes exactly according to your strategy guide? 
+    ${answer2}
+`);
